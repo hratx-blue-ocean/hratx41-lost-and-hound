23fd675d31c4f1d6d2db049fa6ede475f259b52d
Chad Cramer <chcr6493@colorado.edu>
2019-06-25 15:58:57 -0500

    Working on header

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,7 +5,15 @@ import { Navbar } from "react-bootstrap";
 const Header = props => {
   return (
     <Navbar sticky="top" bg="light" expand="lg">
-      <Navbar.Brand>Lost And Hound</Navbar.Brand>
+      <Navbar.Brand className="navTitle" onClick={props.homeRedirect}>
+        Lost And Hound
+      </Navbar.Brand>
+      <a class="nav-link" href="#">
+        About
+      </a>
+      <a class="nav-link" href="#">
+        Resources
+      </a>
     </Navbar>
   );
 };
