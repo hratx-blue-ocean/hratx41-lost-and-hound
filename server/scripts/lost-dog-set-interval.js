77d15a397272fc19273993c983761984dc9d74ff
anthon78 <economicsstudent2000@gmail.com>
2019-06-26 13:56:49 -0500

    add logic to periodically retrieve lost dog info

--- server/scripts/lost-dog-set-interval.js
+++ server/scripts/lost-dog-set-interval.js
@@ -1,44 +1,5 @@
-const createError = require("http-errors");
-const logger = require("morgan");
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const app = express();
-
-// app.set("view engine", "html");
-//commment test
-// open up CORS
-
-app.use(cors());
-
-app.use(logger("dev"));
-
-// You can place your routes here, feel free to refactor:
-const { foundDogs, lostDogs } = require("./routes");
-app.use(express.static(path.join(__dirname, "../client/public")));
-app.use("/api/found", foundDogs);
-app.use("/api/lost", lostDogs);
-
-// app.get("/", (req, res) => {
-//   res.send("HELLO");
-// });
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
 function intervalDogs() {
-  setInterval(getLostDogs, 3600000);
+  setInterval(getLostDogs, 5000);
 }
 
 function getLostDogs() {
@@ -139,7 +100,4 @@ function getLostDogs() {
     });
 }
 
-intervalDogs();
-
-
-module.exports = app;
+export default intervalDogs;
