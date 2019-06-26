493894c2233893a4ad30dfbe21888c2ede23cca3
Ken <kenmarut@gmail.com>
2019-06-26 11:21:07 -0500

    feat(dog.js): Add a /api/dog/:id route
    Added route and imported into server, still needs to be implemented with db

---
+++ server/routes/dog.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+router.get("/dog/:id", (req, res) => {
+  const mongoId = req.params.id;
+  // retrieve data from mongo
+  res.json(mongoId);
+});
+
+module.exports = router;
