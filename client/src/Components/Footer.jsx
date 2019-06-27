0326ff2351a2bdb18e2fe6ae959917db31707715
Ken <kenmarut@gmail.com>
2019-06-27 10:15:09 -0500

    styled footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,16 +3,16 @@ import "../Styles/footer.scss";
 
 const Footer = props => {
   return (
-    <div className="footer">
-      <footer className="page-footer font-small blue pt-4">
-        <div className="footer-copyright text-center py-3">
+    <>
+      <footer className="page-footer font-small blue">
+        <div id="footer-text" className="footer-copyright text-center">
           © 2019 pawpyright:
           <a href="https://mdbootstrap.com/education/bootstrap/">
             &nbsp;lost and hound
           </a>
         </div>
       </footer>
-    </div>
+    </>
   );
 };
 
