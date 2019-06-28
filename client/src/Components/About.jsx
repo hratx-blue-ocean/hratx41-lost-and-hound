e2a45a71b24876b8d9de95f8a79028cf18a4b912
Ken <kenmarut@gmail.com>
2019-06-28 17:30:32 -0500

    fixed styling

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "../Styles/splash.scss";
 import "../Styles/about.scss";
 
@@ -8,31 +9,28 @@ let picture = () => {
 
 const About = props => {
   return (
-    <div className="jumbotron" id="about-container">
-      <h3 className="mission">Our Mission</h3>
-      <h6 className="mission">"Empower dogs"</h6>
-      <h1 className="display-4">About</h1>
-      <hr className="my-4" />
-      <p>
-        {" "}
-        Lost and Hound is committed to helping you find your lost animals. We
-        understand the pain of losing your best friend and the stress of finding
-        someone elses. Our mission is to use the power of technology to bring
-        people who care about their pets together.
-      </p>
-      <a
-        className="btn btn-primary btn-lg"
-        href="#"
-        role="button"
-        onClick={() => {
-          window.open(
-            "https://images.beano.com/store/5158913ed4f44b801ce1019926f3da2fa05a43be18a73ef71d5c62a93da7?auto=compress&w=960&fit=max"
-          );
-        }}
-      >
-        Learn more
-      </a>
-    </div>
+    <Container id="about-container">
+      <Row>
+        <Col sm={2} />
+        <Col id="center-column" sm={8}>
+          <div id="about-contents">
+            <h2 className="mission">Our Mission</h2>
+            <h6 className="mission">
+              <strong>Empower dogs</strong>
+            </h6>
+            <p>
+              {" "}
+              Lost and Hound is committed to helping you find your lost animals.
+              We understand the pain of losing your best friend and the stress
+              of finding someone elses. Our mission is to use the power of
+              technology to bring people who care about their pets together.
+            </p>
+            <img src="http://localhost:8000/assets/aboutus.jpg" />
+          </div>
+        </Col>
+        <Col sm={2} />
+      </Row>
+    </Container>
   );
 };
 
--- client/src/Components/Splash.jsx
+++ client/src/Components/Splash.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import "../Styles/splash.scss";
 import Banner from "./Banner.jsx";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row, Container } from "react-bootstrap";
 
 const Splash = props => {
   return (
     <>
       <div className="splashContainer">
         <Banner />
-        <div id="action-selector">
-          <div>
-            <Button
-              id="splashButton"
-              onClick={props.clickHandler}
-              value="Lost"
-              variant="light"
-            >
-              I lost a dog
-            </Button>
-            <Button
-              id="splashButton"
-              onClick={props.clickHandler}
-              value="Found"
-              variant="light"
-            >
-              I found a dog
-            </Button>
-          </div>
+      </div>
+      <div id="action-selector">
+        <div>
+          <Button
+            id="splashButton"
+            onClick={props.clickHandler}
+            value="Lost"
+            variant="light"
+          >
+            I lost my dog
+          </Button>
+          <Button
+            id="splashButton"
+            onClick={props.clickHandler}
+            value="Found"
+            variant="light"
+          >
+            I found a dog
+          </Button>
         </div>
       </div>
     </>
