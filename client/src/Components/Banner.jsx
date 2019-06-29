092c2718f88a1202ee395a1e60b573b91a71b187
Ken <kenmarut@gmail.com>
2019-06-29 12:31:02 -0500

    styling round 2

--- client/src/Components/Banner.jsx
+++ client/src/Components/Banner.jsx
@@ -3,13 +3,15 @@ import "../Styles/banner.scss";
 import Logo from "./Assets/Logo.jsx";
 
 const Banner = () => (
-  <div id="banner">
-    <Logo id="logo" />
-    <div id="title">
-      <h1 id="company-name">lost and hound</h1>
-      <h3 id="sub-title">bringing pet families back together</h3>
+  <>
+    <div id="banner">
+      <Logo id="logo" />
+      <div id="company-details">
+        <h1 id="company-name">lost and hound</h1>
+        <h3 id="sub-title">bringing pet families back together</h3>
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Banner;
